dd89242824098c01c7349dbf9879729949a74ec5
Mehmet Göksu <1030510150@erciyes.edu.tr>
2021-07-11 10:26:06 +0300

    Add project files.

---
+++ Uyg1Randevu/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uyg1Randevu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string tckimlikNo = textBox1.Text;
+            string adi = textBox2.Text;
+            string soyadi = textBox3.Text;
+            string klinik = comboBox1.SelectedItem.ToString();
+            string tarih = dateTimePicker1.Value.ToShortDateString();
+            string saat = comboBox2.SelectedItem.ToString();
+            string satir = tckimlikNo + "*" + adi + "*" + soyadi + "*" + klinik + "*" + tarih + "*" + saat;
+            //textBox4.Text += satir+Environment.NewLine;
+            try
+            {
+                if (Randevu.RandevuVarMi(tckimlikNo,klinik, tarih, saat) == true)
+                    MessageBox.Show("Sectiginiz Bölümde sectiginiz tarih ve saat dolu");
+                else
+                Randevu.Ekle(satir);
+            }
+            catch
+            {
+                MessageBox.Show("Eklenemedi");
+            }
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string ricerik = Randevu.tumRandevular();
+            textBox4.Text = ricerik;
+        }
+    }
+}
---
+++ Uyg1Randevu/Randevu.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Uyg1Randevu
+{
+    class Randevu
+    {
+        static string dosyaYolu = @"randevu.txt";//@:escape karakteri
+        public static void Ekle(string satir)
+        {
+                StreamWriter yazmaNesnesi=yazmaNesnesi= new StreamWriter(dosyaYolu, true);
+                yazmaNesnesi.WriteLine(satir);
+                MessageBox.Show("Eklendi");
+                yazmaNesnesi.Close();
+ 
+        }
+
+        public static string tumRandevular()
+        {
+            StreamReader okumaNesnesi = new StreamReader(dosyaYolu);
+            //string icerik = okumaNesnesi.ReadToEnd();
+            string icerik = "";
+            while (okumaNesnesi.EndOfStream == false)
+            {
+                icerik += okumaNesnesi.ReadLine()+Environment.NewLine;
+            }
+            okumaNesnesi.Close();
+            return icerik;
+
+        }
+
+        public static bool RandevuVarMi(string kimlik,string bolum,string tarih, string saat)
+        {
+            StreamReader okumaNesnesi = new StreamReader(dosyaYolu);
+            string satir = "";
+            bool sonuc = false;
+            while (okumaNesnesi.EndOfStream == false&&sonuc==false)
+            {
+                satir = okumaNesnesi.ReadLine();
+                string[] parcalar = satir.Split('*');
+                if ((parcalar[3] == bolum) && (parcalar[4] == tarih) && (parcalar[5] == saat))
+                    sonuc = true;
+                if ((parcalar[0] == kimlik) && (parcalar[3] == bolum) && (parcalar[4] == tarih))
+                    sonuc = true;
+            }
+            okumaNesnesi.Close();
+            return sonuc;
+        }
+    }
+}
